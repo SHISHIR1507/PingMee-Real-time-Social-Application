ea15a10db530a263e5748a8108bc6aca0f95a195
SHISHIR1507 <singhshishir4727@gmail.com>
2025-08-25 08:04:48 +0530

    Remove duplicate backend files from root - keep only in backend folder

--- src/routes/chat.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-import { protectRoute } from "../middlewares/auth.middleware.js"
-import { getStreamToken } from "../controllers/chat.controllers.js"
-
-const router = express.Router()
-router.get("/token", protectRoute, getStreamToken)
-
-export default router
