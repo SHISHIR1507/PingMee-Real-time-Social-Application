3a741c454467a456203bf59b00e959fb8d38a30c
SHISHIR1507 <singhshishir4727@gmail.com>
2025-08-28 05:15:51 +0530

    chat-loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,7 +56,17 @@ const App = () => {
 
         ) } />
 
-        <Route path="/chat" element={isAuthenticated ? <ChatPage />: <Navigate to="/login" />} />
+        <Route path="/chat/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <ChatPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
 
         <Route path="/call" element={isAuthenticated ? <CallPage />: <Navigate to="/login" />} />
       </Routes>
