51e571c1b0ec45486c71526e239a5e59896e6836
SHISHIR1507 <singhshishir4727@gmail.com>
2025-08-28 20:28:57 +0530

    backend

--- backend/src/controllers/chat.controllers.js
+++ backend/src/controllers/chat.controllers.js
@@ -4,7 +4,7 @@ const getStreamToken=async(req,res)=>{
         const token=generateStreamToken(req.user.id)
         res.status(200).json({token})
     } catch (error) {
-        console.error("Error fetching Stream token:", error)
+        console.error("Error fetching Stream token:", error.message);
         res.status(500).json({message:"Internal server error"})
     }
 }
